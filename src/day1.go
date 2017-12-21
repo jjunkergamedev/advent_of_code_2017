d41b443e37806cf77f4147fca7233ff2df88466a
Jonathon Junker <jjunkergamedev@gmail.com>
2017-12-21 06:50:25 -0800

    Finished Day 1 Part 2

--- src/day1.go
+++ src/day1.go
@@ -17,27 +17,49 @@ func num(v byte) int {
 	return int(v - byte('0'));
 }
 
-func main() {
-	// f, err := os.Open("day1_input.txt")
-	// check(err)
+func part1(data []byte) {
+	fmt.Println("Part 1: Calculating Checksum...")
 
-	dat, err := ioutil.ReadFile("day1_input.txt")
-	check(err)
-	fmt.Println(string(dat))
-
-	fmt.Println("Calculating Checksum...")
-
-	// prev := strconv.ParseInt(dat[len(dat)-1])
-	// fmt.Println("prev: ", prev)
 	sum := 0
-	prev := dat[len(dat) - 1]
-	for i, s := range dat {
+	prev := data[len(data) - 1]
+	for _, s := range data {
 		if(s == prev) {
-			fmt.Println("i: ", i, " s: ", num(s), "prev: ", num(prev))
+			// fmt.Println("i: ", i, " s: ", num(s), "prev: ", num(prev))
 			sum += num(s)
 		}
 		prev = s
 	}
 
-	fmt.Println("sum: ", sum)
+	fmt.Println("Part 1 sum: ", sum)
+}
+
+func part2(data []byte) {
+	fmt.Println("Part 2: Calculating Checksum...")
+
+	sum := 0
+	offset := len(data) / 2
+	fmt.Println("offset: ", offset)
+	for i, s := range data {
+		other := data[(i + offset) % len(data)]
+		if(s == other) {
+			// fmt.Println("i: ", i, " s: ", num(s), "other: ", num(other))
+			sum += num(s)
+		}
+	}
+
+	fmt.Println("Part 2 sum: ", sum)
+}
+
+func main() {
+	dat, err := ioutil.ReadFile("day1_input.txt")
+	check(err)
+	fmt.Println(string(dat))
+
+	part1(dat)
+
+	dat, err = ioutil.ReadFile("day1_input_2.txt")
+	check(err)
+	fmt.Println(string(dat))
+
+	part2(dat)
 }
